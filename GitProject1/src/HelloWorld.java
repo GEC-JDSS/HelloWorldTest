3e6ba20d0776068ce6055b8b76d7c1334bfc301e
Margaret Green <mgreen@hfcc.edu>
2017-01-17 14:45:43 -0500

    Added third line in Eclipse

--- GitProject1/src/HelloWorld.java
+++ GitProject1/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Again, I say Hello");
+		System.out.println("Adding a line in Eclipse");
 	}
 
 }
